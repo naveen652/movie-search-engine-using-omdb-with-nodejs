024088ecd8b6dd64702a39c562dbb69bb3994efb
naveen652 <73928844+naveen652@users.noreply.github.com>
2023-08-06 19:07:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('search');
+});
+
+app.get('/results', function(req, res){
+    var query = req.query.search;
+    var url = 'https://www.omdbapi.com/?s=' + query + '&apikey=19cfb24b';
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body)
+            res.render('results', {data: data});
+        }
+    });
+});
+
+ app.listen(3000, function(){
+     console.log('Movie app started on port: 3000');
+ });
